5f166f8afa46b47fb2f0e04e077d51ca3257c041
josefineluther <josefineluther@hotmail.se>
2025-11-18 22:09:35 +0100

    project-information

--- src/pages/ProjectDetail.tsx
+++ src/pages/ProjectDetail.tsx
@@ -18,7 +18,8 @@ const projects = [
     projectImage: '',
     description:
       'Ongoing project. Me and a friend from school are building a platform where creative people can share their creations such as crocheting, drawings and DIY projects. More info coming soon!',
-    date: '2025-'
+    date: '2025-',
+    github: 'https://github.com/josefineluther/solvingpyssels'
   },
   {
     id: 1,
@@ -27,15 +28,17 @@ const projects = [
     projectImage: bokhyllanImg,
     description:
       'This website features a homepage with recommended books, a full catalog with genre filters, individual book pages, and a shopping cart that calculates discounts such as “3 for 2.” The frontend handles cart functionality, including adding, removing, and grouping books, while the backend manages database requests and filtering. I generated all book covers using ChatGPT to avoid using real books and implemented a mobile-friendly design with a hamburger menu. This project gave me valuable experience in connecting frontend and backend systems, debugging, and structuring a full-stack application.',
-    date: '2025'
+    date: '2025',
+    github: 'https://github.com/josefineluther/bokhyllan'
   },
   {
     id: 2,
     title: 'WordBond',
     tags: ['Vue', 'School project'],
     projectImage: wordbondImg,
-    description: '',
-    date: '2025'
+    description: 'This was a group project where we created a website that connects people who want to learn and teach languages with users from other countries. We built a start page with a search function, user account creation, profile editing, and an integrated chat feature.',
+    date: '2025',
+    github: 'https://github.com/josefineluther/wordbond'
   },
   {
     id: 3,
@@ -44,31 +47,35 @@ const projects = [
     projectImage: galleriGoghImg,
     description:
       'Galleri Gogh is an art gallery website that showcases various artworks fetched from the Met Museum REST API. The site features a clean and modern design, allowing users to browse through different art pieces and view detailed information about each artwork. This project helped me enhance my skills in working with APIs, managing state in React, and creating visually appealing layouts.',
-    date: '2025'
+    date: '2025',
+    github: ''
   },
   {
     id: 4,
     title: 'Skiathos Cat Shelter',
     tags: ['JavaScript', 'REST API', 'School project'],
     projectImage: skiathosCatsImg,
-    description: '',
-    date: '2024'
+    description: 'I built an interactive website for a Greek cat shelter. Users can search for cat breeds through an external API, view results with images, and access detailed breed pages where the page title updates dynamically. I added logic to prevent outdated search results caused by API response delays. The site also includes a statistics page using Chart.js, where I visualized intelligence levels across selected breeds fetched via the API. The project combines structured HTML with JavaScript for API handling, dynamic rendering, and data visualization.',
+    date: '2024',
+    github: 'https://github.com/josefineluther/Skiathoscats'
   },
   {
     id: 5,
     title: 'Decthings',
     tags: ['Web design', 'Figma'],
     projectImage: decthingsImg,
-    description: '',
-    date: '2024'
+    description: 'I designed the website for the startup company Decthings, along with their logo and graphical profile. I worked in Adobe Illustrator and Figma.',
+    date: '2024',
+    github: ''
   },
   {
     id: 6,
     title: 'Apoceus',
     tags: ['Web design', 'Figma'],
     projectImage: apoceusImg,
-    description: '',
-    date: '2024'
+    description: 'I worked in a project where I, together with a team, designed the website to the upcoming game Apoceus by Landell Games. We created a mockup in Figma and collaborated with the developers, who implemented the final product.',
+    date: '2024',
+    github: ''
   }
 ]
 
@@ -115,6 +122,8 @@ const ProjectDetail = () => {
           <div className='flex flex-col justify-center space-y-6'>
 
             <p className='text-foreground/80 leading-relaxed text-sm leading-7'>{project.description}</p>
+
+            { project.github && <a className='text-xs text-muted-foreground hover:opacity-60' href={project.github} target="_blank">View on GitHub</a> }
 
             <div className='flex flex-wrap gap-2 text-xs text-muted-foreground pt-4 border-t-2 border-secondary'>
               {project.tags.map((tag, tagIndex) => (
